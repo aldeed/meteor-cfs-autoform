d5e9325e3410d3ccb26a9c5cc848493788ed9510
Eric Dobbertin <eric@dairystatedesigns.com>
2014-10-07 07:53:30 -0500

    fixes, fully commented

--- cfs-autoform.js
+++ cfs-autoform.js
@@ -1,5 +1,6 @@
 if (Meteor.isClient) {
 
+  // Adds a custom "cfs-file" input type that AutoForm will recognize
   AutoForm.addInputType("cfs-file", {
     componentName:"cfsFileField",
     valueOut: {
@@ -15,6 +16,7 @@ if (Meteor.isClient) {
     }
   });
 
+  // Adds a custom "cfs-files" input type that AutoForm will recognize
   AutoForm.addInputType("cfs-files", {
     componentName:"cfsFilesField",
     valueIsArray: true,
@@ -67,7 +69,7 @@ if (Meteor.isClient) {
     }
   };
 
-  var handler = function (event, template) {
+  var singleHandler = function (event, template) {
     var fileList = [];
     FS.Utility.eachFile(event, function (f) {
       fileList.push(f.name);
@@ -82,16 +84,37 @@ if (Meteor.isClient) {
     'click .cfsaf-field': function (event, template) {
       template.$('.cfsaf-hidden').click();
     },
-    'change .cfsaf-hidden': handler,
-    'dropped .cfsaf-field': handler
+    'change .cfsaf-hidden': singleHandler,
+    'dropped .cfsaf-field': singleHandler
   });
+
+  var multipleHandler = function (event, template) {
+    // Get the FileList object from the event object
+    var fileList = event.originalEvent.dataTransfer ? event.originalEvent.dataTransfer.files : event.currentTarget.files;
+    
+    // Store the FileList on the file input for later. We store an array of
+    // separate FileList objects. Browsers don't let you add/remove items from
+    // an existing FileList.
+    var fileListList = template.$('.cfsaf-hidden').data("cfsaf_files_multi") || [];
+    fileListList.push(fileList);
+    template.$('.cfsaf-hidden').data("cfsaf_files_multi", fileListList);
+
+    // Get full list of files to display in the visible, read-only field
+    var fullNameList = [];
+    _.each(fileListList, function (fileList) {
+      _.each(fileList, function (f) {
+        fullNameList.push(f.name);
+      });
+    });
+    template.$('.cfsaf-field').val(fullNameList.join(", "));
+  };
 
   Template["cfsFilesField_bootstrap3"].events({
     'click .cfsaf-field': function (event, template) {
       template.$('.cfsaf-hidden').click();
     },
-    'change .cfsaf-hidden': handler,
-    'dropped .cfsaf-field': handler
+    'change .cfsaf-hidden': multipleHandler,
+    'dropped .cfsaf-field': multipleHandler
   });
 
   function deleteUploadedFiles(template) {
@@ -115,71 +138,139 @@ if (Meteor.isClient) {
             return false;
           }
 
+          // Loop through all hidden file inputs in the form.
           var totalFiles = 0;
+          var arrayFields = {};
           template.$('.cfsaf-hidden').each(function () {
             var elem = $(this);
-            var files = elem.data("cfsaf_files");
-            if (files) {
-              totalFiles += files.length;
+
+            // Get schema key that this input is for
+            var key = elem.attr("data-schema-key");
+
+            // no matter what, we want to delete the dummyId value
+            delete doc[key];
+
+            // Get list of files that were attached for this key
+            var fileList = elem.data("cfsaf_files");
+
+            // If we have some attached files
+            if (fileList) {
+              // add all files to total count
+              totalFiles += fileList.length;
             }
 
-            // delete the dummyId value
-            var key = elem.attr("data-schema-key");
-            delete doc[key];
+            // Otherwise it might be a multiple files field
+            else {
+              var fileListList = elem.data("cfsaf_files_multi");
+              if (fileListList) {
+                // make a note that it's an array field
+                arrayFields[key] = true;
+                // add all files to total count
+                _.each(fileListList, function (fileList) {
+                  totalFiles += fileList.length;
+                });
+                // prep the array
+                doc[key] = [];
+              }
+            }
           });
 
+          // If no files were attached anywhere on the form, we're done.
+          // We pass back the doc synchronously
           if (totalFiles === 0) {
             return doc;
           }
           
+          // Create the callback that will be called either
+          // upon file insert error or upon each file being uploaded.
           var doneFiles = 0;
           var failedFiles = 0;
           function cb(error, fileObj, key) {
+            // Increment the done files count
             doneFiles++;
+
+            // Increment the failed files count if it failed
             if (error) {
               failedFiles++;
-            } else {
-              // set real ID of uploaded file into the doc
-              doc[key] = fileObj._id;
             }
+
+            // If it didn't fail, set the new ID as the property value in the doc,
+            // or push it into the array of IDs if it's a multiple files field.
+            else {
+              if (arrayFields[key]) {
+                doc[key].push(fileObj._id);
+              } else {
+                doc[key] = fileObj._id;
+              }
+            }
+
+            // If this is the last file to be processed, pass execution back to autoform
             if (doneFiles === totalFiles) {
+              // If any files failed
               if (failedFiles > 0) {
-                // if any failed, we delete all that succeeded
+                // delete all that succeeded
                 deleteUploadedFiles(template);
                 // pass back to autoform code, telling it we failed
                 self.result(false);
-              } else {
+              }
+              // Otherwise if all files succeeded
+              else {
                 // pass updated doc back to autoform code, telling it we succeeded
                 self.result(doc);
               }
             }
           }
 
+          // Loop through all hidden file fields, inserting
+          // and uploading all files that have been attached to them.
           template.$('.cfsaf-hidden').each(function () {
             var elem = $(this);
+
+            // Get schema key that this input is for
             var key = elem.attr("data-schema-key");
+
+            // Get the FS.Collection instance
             var fsCollectionName = elem.attr("data-cfs-collection");
-            var files = elem.data("cfsaf_files");
-            _.each(files, function (file) {
-              var fileObj = new FS.File(file);
-              fileObj.once("uploaded", function () {
-                // track successful uploads so we can delete them if any
-                // of the other files fail to upload
-                var uploadedFiles = elem.data("cfsaf_uploaded-files") || [];
-                uploadedFiles.push(fileObj);
-                elem.data("cfsaf_uploaded-files", uploadedFiles);
-                // call callback after uploaded, not just inserted
-                cb(null, fileObj, key);
-              });
-              FS._collections[fsCollectionName].insert(fileObj, function (error, fileObj) {
-                if (error) {
+            var fsCollection = FS._collections[fsCollectionName];
+
+            // Loop through all files that were attached to this field
+            function loopFileList(fileList) {
+              _.each(fileList, function (file) {
+                // Create the FS.File instance
+                var fileObj = new FS.File(file);
+
+                // Listen for the "uploaded" event on this file, so that we
+                // can call our callback. We want to wait until uploaded rather
+                // than just inserted. XXX Maybe should wait for stored?
+                fileObj.once("uploaded", function () {
+                  // track successful uploads so we can delete them if any
+                  // of the other files fail to upload
+                  var uploadedFiles = elem.data("cfsaf_uploaded-files") || [];
+                  uploadedFiles.push(fileObj);
+                  elem.data("cfsaf_uploaded-files", uploadedFiles);
+                  // call callback
+                  cb(null, fileObj, key);
+                });
+
+                // Insert the FS.File instance into the FS.Collection
+                fsCollection.insert(fileObj, function (error, fileObj) {
                   // call callback if insert/upload failed
-                  cb(error, fileObj, key);
-                }
-                // TODO progress bar during uploads
+                  if (error) {
+                    cb(error, fileObj, key);
+                  }
+                  // TODO progress bar during uploads
+                });
               });
+            }
+
+            // single fields first
+            loopFileList(elem.data("cfsaf_files"));
+            // then multiple fields
+            _.each(elem.data("cfsaf_files_multi"), function (fileList) {
+              loopFileList(fileList);
             });
           });
+
         }
       },
       after: {
@@ -193,6 +284,7 @@ if (Meteor.isClient) {
           } else {
             // cleanup files data
             elems.removeData("cfsaf_files");
+            elems.removeData("cfsaf_files_multi");
           }
           // cleanup uploaded files data
           elems.removeData("cfsaf_uploaded-files");
