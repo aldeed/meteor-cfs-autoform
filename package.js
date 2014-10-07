11f5f968d2df9d275d700bf4fc4ae6676c200415
Eric Dobbertin <eric@dairystatedesigns.com>
2014-10-07 08:05:22 -0500

    bump version

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: "cfs:autoform",
-  version: "2.0.0",
+  version: "2.0.0-rc1",
   summary: "Upload files as part of autoform submission",
   git: "https://github.com/aldeed/meteor-cfs-autoform.git"
 });
@@ -9,7 +9,7 @@ Package.on_use(function(api) {
   api.versionsFrom('METEOR@0.9.3');
   api.use(['underscore', 'templating'], 'client');
 
-  api.use('aldeed:autoform@4.0.0');
+  api.use('aldeed:autoform@4.0.0-rc1');
   api.use('cfs:standard-packages@0.0.2');
   api.use('raix:ui-dropped-event@0.0.7', 'client');
 
