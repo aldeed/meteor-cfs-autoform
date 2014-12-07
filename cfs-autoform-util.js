f5921cbc47ef4d0f815b24ae666ca785c7adf0a9
David Pankros <dpankros@miragy.com>
2014-12-07 17:05:46 -0500

    Refactored hook code to cfs-autoform-hooks Added utility code for deep finding, setting, and getting of properties in cfs-autoform-utils added global object CfsAutoForm for access these functions

---
+++ cfs-autoform-util.js
@@ -0,0 +1,30 @@
+Util = {
+  //delete prop from obj
+  //prop can be something like "obj.3.badprop
+  deepDelete: function(obj, prop){
+    return Utilities.deepDo(obj, prop, function(obj, prop){
+      delete obj[prop];
+    });
+  },
+  deepSet: function(obj, prop, value){
+    return Utilities.deepDo(obj, prop, function(obj, prop){
+      obj[prop] = value;
+    });
+  },
+  //returns the object that CONTAINS the last property
+  deepFind: function(obj, prop){
+    path = path.split('.');
+    for (i = 0; i < path.length - 1; i++)
+      obj = obj[path[i]];
+
+    return obj;
+  },
+  //executes closure(obj, prop) where prop might be a string of properties and array indices
+  deepDo: function(obj, path, closure){
+    path = path.split('.');
+    for (i = 0; i < path.length - 1; i++)
+      obj = obj[path[i]];
+
+    closure.apply(this, [obj, path[i]]);
+  }
+};
